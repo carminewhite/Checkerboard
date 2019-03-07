22fafb58b9b92e473d6ddd1e467a1a19f8cc6ad7
Carmine White <c.white@ahkclean.com>
2019-03-07 11:58:40 -0800

    finished checkerboard (not ninja or sensei version)

--- server.py
+++ server.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/<red>')
-def red_black_board(red):
-    print("\n" + "*"*50+"\nIn the 'redblack' function")
-    return render_template('index.html', color = red)
+
+@app.route('/<red_or_green>')
+def chess_board(red_or_green):
+    primary_color = red_or_green
+    other_color = "#ffffe0"
+    if primary_color == "red":
+        other_color = "black"
+    elif primary_color == "green":
+        other_color = "#ffffe0"
+
+    print("\n" + "*"*50+"\nIn the 'chess_board' function.  Color is:", red_or_green)
+    return render_template('index.html', primary_color = primary_color, other_color = other_color)
+
 
 if __name__=="__main__":
     app.run(debug=True)
